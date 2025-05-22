7e0a0d10dc5c50659eed9efc3bb98f907d7bd568
h4ck3r-04 <mr.rudradityathakur@gmail.com>
2025-05-22 18:55:36 +0530

    fixed: build error

--- app/(app)/docs/[[...slug]]/page.tsx
+++ app/(app)/docs/[[...slug]]/page.tsx
@@ -1,23 +1,49 @@
-import { DocsContent } from "./_components/docs-content";
-import { notFound } from "next/navigation";
-import { Content } from "@/types/content";
+import type { Metadata, ResolvingMetadata } from 'next';
+import { notFound } from 'next/navigation';
+import { DocsContent } from './_components/docs-content';
+import { Content } from '@/types/content';
+
+type Props = {
+  params: Promise<{ slug?: string[] }>;
+};
 
 async function getContentFromSlug(slugPath: string): Promise<Content | null> {
   try {
     const contentModule = await import(`@/content/${slugPath}`);
     return contentModule.default as Content;
   } catch (err) {
-    console.error("Failed to load content:", slugPath, err);
+    console.error('Failed to load content:', slugPath, err);
     return null;
   }
 }
 
-export default async function DocPage({
-  params,
-}: {
-  params: { slug?: string[] };
-}) {
-  const slugPath = params.slug?.length ? params.slug.join("/") : "introduction";
+export async function generateMetadata(
+  { params, }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const { slug } = await params;
+  const slugPath = slug?.join('/') || 'introduction';
+  const content = await getContentFromSlug(slugPath);
+
+  if (!content) {
+    return {
+      title: 'Page Not Found',
+    };
+  }
+
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: content.title,
+    openGraph: {
+      images: ['/docs-default-image.jpg', ...previousImages],
+    },
+  };
+}
+
+export default async function Page({ params }: Props) {
+  const { slug } = await params;
+  const slugPath = slug?.join('/') || 'introduction';
   const content = await getContentFromSlug(slugPath);
 
   if (!content) return notFound();
